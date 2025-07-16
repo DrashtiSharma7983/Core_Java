f1e1ff4c1caf1f0a5f9fb218bc89197d0489901e
Drashti <drashtisharma7983@gmail.com>
2025-07-16 14:42:01 +0530

    Adding new files

---
+++ Datatype.java
@@ -0,0 +1,26 @@
+class Datatype{
+    public static void main(String[] a) {
+        int num= 200;
+        byte by = 23;
+        short sh = 567;
+        long lg = 9898729829l;
+
+        float fl = 76.9f;
+        double db = 369846.99;
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        char ch = 'D';
+        System.out.println(num);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(lg);
+        System.out.println(fl);
+        System.out.println(db);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(ch);
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Core_Java
 This is all about Basics of java.
+Here you will gwt to know all about java basics.
