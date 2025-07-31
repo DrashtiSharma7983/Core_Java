65daaa5ef73c676e12bf6df31582495cf45069c5
Drashti <drashtisharma7983@gmail.com>
2025-07-31 09:22:02 +0530

    dowhileloop

---
+++ DoWhileLoop.java
@@ -0,0 +1,11 @@
+class DoWhileLoop{
+    public static void main(String []a){
+        int i=8;
+
+        do{
+            System.out.println("i is : " +i);
+            i++;
+        }while(i<5);
+        System.out.println("out of the loop");
+    }
+}
