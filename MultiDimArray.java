72abb1b857127b76312ee39a99d1b1d41211de9d
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2025-08-31 00:09:53 +0530

    Add files via upload
    Adding MultiDimentional array.

---
+++ MultiDimArray.java
@@ -0,0 +1,26 @@
+public class MultiDimArray {
+    public static void main(String[] args) {
+//        int a[][] = new int[3][3];
+
+//        a[0][0] = 80;
+//        a[0][1] = 50;
+//        a[0][2] = 40;
+//
+//        a[1][0] = 88;
+//        a[1][1] = 57;
+//        a[1][2] = 70;
+//
+//        a[2][0] = 60;
+//        a[2][1] = 78;
+//        a[2][2] = 99;
+
+        //Another way of defining values
+        int marks[][] = {
+                {80,50,40},
+                {40,50,60},
+                {66,33,22}
+        };
+        System.out.println(marks[0][2]);
+    }
+
+}
